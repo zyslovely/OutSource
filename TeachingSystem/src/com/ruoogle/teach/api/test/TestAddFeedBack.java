22990d2859b6586624ed781c7bfb98910c09acac
Binying Qiu <yunshang_734@163.com>
2013-07-17 22:40:01 +0800

    搜索接口

--- TeachingSystem/src/com/ruoogle/teach/api/test/TestAddFeedBack.java
+++ TeachingSystem/src/com/ruoogle/teach/api/test/TestAddFeedBack.java
@@ -5,7 +5,7 @@ import org.apache.commons.httpclient.methods.PostMethod;
 
 public class TestAddFeedBack {
 	public static void main(String[] args) {
-		String strURL = "http://teach.zys-wings.com/teach/api/feedback/add/?toUserId=1&feedbackId=1&content=hello&courseId=1&token=vzMiFI7h8er4YegcjtmVdz4I73TN8wEz";
+		String strURL = "http://teach.zys-wings.com/teach/api/feedback/add/?toUserId=1&feedbackId=1&content=hello&courseId=1&token=vzMiFI7h8eqfJXbNvkIrmItyagxMQloT";
 		// Get file to be posted
 		HttpClient httpclient = new HttpClient();
 		PostMethod post = new PostMethod(strURL);
--- TeachingSystem/src/com/ruoogle/teach/api/test/TestGetFeedBackList.java
+++ TeachingSystem/src/com/ruoogle/teach/api/test/TestGetFeedBackList.java
@@ -5,7 +5,7 @@ import org.apache.commons.httpclient.methods.PostMethod;
 
 public class TestGetFeedBackList {
 	public static void main(String[] args) {
-		String strURL = "http://teach.zys-wings.com/teach/api/feedback/?userId=1&limit=10&offset=0&token=vzMiFI7h8eo8J%2Fp9EEdnp%2BxIgMb5N6VS";
+		String strURL = "http://teach.zys-wings.com/teach/api/feedback/?userId=1&limit=10&offset=0&token=vzMiFI7h8eqfJXbNvkIrmItyagxMQloT";
 		// Get file to be posted
 		HttpClient httpclient = new HttpClient();
 		PostMethod post = new PostMethod(strURL);
