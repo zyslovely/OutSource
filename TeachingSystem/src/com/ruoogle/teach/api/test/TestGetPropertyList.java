2b84d0e198dd7616ca38374fe6ff8337e2af9231
Binying Qiu <yunshang_734@163.com>
2013-07-18 14:13:15 +0800

    测试接口

---
+++ TeachingSystem/src/com/ruoogle/teach/api/test/TestGetPropertyList.java
@@ -0,0 +1,24 @@
+package com.ruoogle.teach.api.test;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+public class TestGetPropertyList {
+	public static void main(String[] args) {
+		String strURL = "http://teach.zys-wings.com/teach/api/property/list/?token=vzMiFI7h8ep7vAoOjxz8kP6XhJn1H3y4";
+		HttpClient httpclient = new HttpClient();
+		PostMethod post = new PostMethod(strURL);
+		try {
+			int result = httpclient.executeMethod(post);
+			System.out.println("the result of post : " + result);
+			System.out.println(" the response of post : "
+					+ post.getResponseBodyAsString());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("error in post");
+		} finally {
+			post.releaseConnection();
+		}
+	}
+}
